e9c654df1a4e8379a876b29014c386a4d17c4684
sarim <sarimali@live.com>
2025-04-22 17:39:20 +0500

    fixed

--- components/share-link-dialog.tsx
+++ components/share-link-dialog.tsx
@@ -48,7 +48,7 @@ export function ShareLinkDialog({ proposalId }: ShareLinkDialogProps) {
         <Button
           variant="outline"
           size="sm"
-          className="text-white dark:text-white"
+          className="text-black dark:text-white"
         >
           <Share2 className="mr-2 h-4 w-4" />
           Share
